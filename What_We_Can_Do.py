777770cbff35186d0c6e807a796d5a99a95dea66
Victor Shneer <victor.shneer@gmail.com>
2025-07-11 03:00:45 +0300

    edits

--- What_We_Can_Do.py
+++ What_We_Can_Do.py
@@ -8,29 +8,42 @@ st.set_page_config(page_title="Holitica")
 
 st.title("Welcome to Holitica")
 
+st.markdown('''
+#### Who we are
+
+We’re a small team focused on data analytics, software engineering, and machine learning.
+
+''')
+
 st.markdown("""
 #### What We Can Do
 
-We turn your data into clear, actionable insights that drive growth.
-Here’s how we can help your business:
-
-**Understand Your Customers** – segment your audience based on behavior, demographics, and value  
-**Target More Effectively** – design high-performing ad audiences using real data, not guesses  
-**Grow Sales with Insight** – match products, timing, and offers to the right people  
-**Spot Opportunities Early** – identify trends, churn risks, and untapped market segments  
-**Make Data Work for You** – from one-time reports to custom dashboards and automated predictions  
-
-Our methods are powered by experience, common sense and machine learning. We connect Marketing, Technology and Data
+Have a question? We help you find the answer in your data.
+We turn data into clear insights that drive growth.
+Our approach combines experience, common sense, and machine learning.
+We bridge the gap between ***Marketing, Technology, and Data***.
 """)
 
 
 st.markdown("""
-Explore real-world data use cases:
+#### How we do it
+
+We carefully prepare the data through cleaning and preprocessing.
+When needed, we also build custom data pipelines and ETL jobs to ensure reliable and scalable data engineering.
+We explore the data and formulate hypotheses.
+Then we dive deep using statistical methods to validate or reject those hypotheses.
+Iteration after iteration, we extract insights that are not only statistically valid but also practically significant.
+
+Explore real-world data use cases — or try them out with ***your own data***:
 
 - 📊 [Retail Customer Insights](./Retail_Customer_Insights)
-- 🧠 [Customer Persona Uncover](./Customer_Persona_Uncover)
+- 🧠 [Customer Persona Discovery](./Customer_Persona_Discovery)
 - 📉 [Understanding Customer Churn](./Understanding_Customer_Churn)
-- [User Based Recommendations](./User_Based_Recommendations)
+- 🧩 [User Based Recommendations](./User_Based_Recommendations)
 
 """)
 
+st.markdown("""
+Curious to see it in action? Use the email form in the left sidebar to request a ***free demo***.
+
+""")
