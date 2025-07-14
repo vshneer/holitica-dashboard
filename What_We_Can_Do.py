62a82dc86deef1ddc6325d4164a9110e1189bd86
Victor Shneer <victor.shneer@gmail.com>
2025-07-14 20:40:30 +0300

    remove unused

--- What_We_Can_Do.py
+++ What_We_Can_Do.py
@@ -8,11 +8,6 @@ from urllib.parse import parse_qs
 show_side_bar_with_more()
 st.set_page_config(page_title="Holitica")
 
-
-query_params = st.experimental_get_query_params()
-page = query_params.get("page", ["home"])[0]
-
-
 st.title("Welcome to Holitica")
 
 st.markdown('''
